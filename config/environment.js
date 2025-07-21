da8510e666915c8027dbe4a08e07756ef9af6ede
Gauri <gauriagarwal25@gmail.com>
2025-07-21 23:44:05 +0530

    Use process.env instead of env_config.json for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,12 @@
-const env = require('./env_config.json');
-
-module.exports = {
-  google_client_id: env.GOOGLE_CLIENT_ID,
-  google_client_secret: env.GOOGLE_CLIENT_SECRET,
-  google_callback_url: env.DEVELOPMENT_GOOGLE_CALLBACK_URL,
-  session_cookie_key: env.SESSION_COOKIE_KEY,
-  db: env.DEVELOPMENT_DB,
-  website_link: env.DEVELOPMENT_WEBSITE_LINK
+const envConfig = {
+  name: process.env.ENVIRONMENT || 'development',
+  asset_path: process.env.ASSET_PATH,
+  session_cookie_key: process.env.SESSION_COOKIE_KEY,
+  db: process.env.DB,
+  google_client_id: process.env.GOOGLE_CLIENT_ID,
+  google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+  google_callback_url: process.env.GOOGLE_CALLBACK_URL,
+  website_link: process.env.WEBSITE_LINK
 };
+
+module.exports = envConfig;
